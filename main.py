6cb75a1996ea2d4388f03de382e22b7ad7de5b5c
vijaymannava <86002106+vijaymannava@users.noreply.github.com>
2021-07-09 22:16:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,5 +42,12 @@ def predict_flower(
     output = {'flower_class': predict(query_data)}
     return output
 
+@app.post("/feedback_loop", status_code=200)
+def feedback_loop(
+        data: List[FeedbackIn]
+):
+    retrain(data)
+    return {"detail":"Feedback loop successful"}
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host='0.0.0.0', port=8888, reload=True)
